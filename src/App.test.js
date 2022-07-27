1af769b4f4fe2e5ba9308436c034585e25153098
Wadah Mac Kraken <wadah-wleed@hotmail.com>
2022-07-28 01:02:49 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -15,6 +15,6 @@ test('renders learn react link', () => {
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn reacts/i);
   expect(linkElement).toBeInTheDocument();
 });
